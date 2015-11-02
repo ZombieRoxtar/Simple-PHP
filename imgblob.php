b423d0a84bcbf408ca0943658e7de70d89e87c80
ZombieRoxtar <ZombieRoxtar@users.noreply.github.com>
2015-11-02 00:16:03 -0500

    Imgblob can now handle relative paths provided that they are relative to the top-level domain. I'll do the rest later...

--- imgblob.php
+++ imgblob.php
@@ -30,12 +30,32 @@
 		<br />
 		<table border>
 			<?php
+				$doOnce=true;
 				for ($i=0;$i<$tags->length;$i++)
 				{
 					echo "<tr>\n";
 					echo "<td>$i</td>\n";
 					echo "<td>";
 					$src=$tags->item($i)->getAttribute('src');
+					if(substr(strtolower($src),0,4) != 'http')
+					{
+						/* If I'm to append HTTP://whatever then a leading / is needed */
+						if(substr(strtolower($src),0,1) != '/')
+						{
+							$src='/'.$src;
+						}
+						if($doOnce)
+						{
+							/* I need only the protocol/host portion for these pesky URLs */
+							$host=substr($url,0,strpos($url,'/',8));
+							if($host == '')
+							{
+								$host=$url;
+							}
+							$doOnce=false;
+						}
+						$src=$host.$src;
+					}
 					echo "<a href='$src'>\n";
 					echo "<img src='$src'><br />\n";
 					echo "$src</a>\n";
